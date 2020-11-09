5fb1d088a78418f7cc9be1ad608fe3d11e47b945
real-shinza <real.shinza@gmail.com>
2020-11-09 15:23:23 +0900

    ジャンケン追加

--- 20201102/20201102/Main.cpp
+++ 20201102/20201102/Main.cpp
@@ -1,8 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+enum JankenType
+{
+	GU = 1,
+	CHOKI,
+	PA,
+};
+
+enum JankenOutcome
+{
+	WIN,
+	LOSE,
+	DRAW,
+};
+
+JankenOutcome Janken(JankenType player, JankenType npc)
+{
+	printf("グー：1\nチョキ：2\nパー：3\\nn");
+
+	
+	if ((int)player < (int)npc)
+	{
+		if ((int)player == 1 && (int)npc == 3)
+			return LOSE;
+		else
+			return WIN;
+	}
+	else if ((int)player > (int)npc)
+	{
+		if ((int)player == 3 && (int)npc == 1)
+			return WIN;
+		else
+			return LOSE;
+	}
+	else
+	{
+		return DRAW;
+	}
+
+}
 
 int main()
 {
-	printf("おはよう\n");
+	while (0)
+	{
+		int data;
+		scanf("%d\n", &data);
+		int randam = rand() % 3 + 1;
+
+		JankenOutcome jankenKekka = Janken((JankenType)data, (JankenType)(int)randam);
+
+		if (jankenKekka == WIN)
+			printf("win\n\n\n");
+		else if (jankenKekka == LOSE)
+			printf("lose\n\n\n");
+		else
+		{
+			printf("draw\n\n\n");
+		}
+	}
 
 	return 0;
 }
